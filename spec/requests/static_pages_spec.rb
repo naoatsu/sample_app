1441c56ebc5c2d72b472ee6c27c2d4ca2d3e6c14
Naoatsu Kurata <naot11111111@gmail.com>
2015-05-19 13:24:14 +0900

    Make a basic User model (includeng secure passwords)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,6 +4,11 @@ describe "StaticPages" do
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_content(heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
   describe "Home page" do
     before { visit root_path }
     it { should have_content ('Sample App') }
