3c3115dccb3b56ff1a05feaf60475d903208b940
Naoatsu Kurata <naot11111111@gmail.com>
2015-06-23 13:24:32 +0900

    Add user following

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,8 +15,13 @@ describe User do
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
-
+  it { should respond_to(:feed) }
+  it { should respond_to(:relationships) }
+  it { should respond_to(:followed_users) }
+  it { should respond_to(:following?) }
+  it { should respond_to(:follow!) }
   it { should be_valid }
+  
 
   describe "when name is not present" do
     before { @user.name = " " }
@@ -101,6 +106,17 @@ describe User do
   describe "remember token" do
     before { @user.save }
     its(:remember_token) { should_not be_blank }
+  end
+  
+  describe "following" do
+    let(:other_user) { FactoryGirl.create(:user) }
+    before do
+      @user.save
+      @user.follow!(other_user)
+    end
+
+    it { should be_following(other_user) }
+    its(:followed_users) { should include(other_user) }
   end
 
 end
